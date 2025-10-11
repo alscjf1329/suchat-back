289658b13c4f5343357fd8004d0d59e8350f0840
SheepDuck <alscjf1329@gmail.com>
2025-10-11 22:21:10 +0900

    fix: 변수 값 export로 변경

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,5 @@
-root_dir=/c/Users/SheepDuck/Desktop/project/suchat/suchat-back
-env_file=$root_dir/.env
+export root_dir=/c/Users/SheepDuck/Desktop/project/suchat/suchat-back
+export env_file=$root_dir/.env
 
 if [ -f $env_file ]; then
     source $env_file
